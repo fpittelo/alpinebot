3a914bef9396ede467b24026715f527e0eaa63f7
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-19 17:39:06 +0200

    #10 update provider to see if...

--- dev/providers.tf
+++ dev/providers.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     azurerm   = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.8"  # Use the appropriate version
+      version = "~> 4.1.0"  # Use the appropriate version
     }
   }
 }
