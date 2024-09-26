ff391da356704b3f4d00cb0fb34ceb763fc48687
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-09-26 13:15:01 +0200

    #23 correction to delete rg

--- infra/providers.tf
+++ infra/providers.tf
@@ -21,6 +21,9 @@ terraform {
 
 provider "azurerm" {
   features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }    
     key_vault {
       purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = true
