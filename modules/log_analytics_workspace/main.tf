44ece801071a9a8d2815562fe98f8f2fed5cb5d4
fpittelo <frederic.pitteloud@gmail.com>
2025-11-21 09:51:12 +0100

    #45 solving duplication

--- modules/log_analytics_workspace/main.tf
+++ modules/log_analytics_workspace/main.tf
@@ -6,11 +6,3 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   retention_in_days   = var.log_analytics_workspace_retention_in_days
   tags                = var.tags
 }
-
-output "log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.id
-}
-
-output "log_analytics_workspace_name" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.name
-}
